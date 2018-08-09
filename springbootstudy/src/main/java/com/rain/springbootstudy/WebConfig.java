cdf918bf5db0ec85e1f0eabc6e76681932e2eecc
jingyu323 <xzg323@163.com>
2018-08-09 19:11:32 +0800

    添加拦截器

--- springbootstudy/src/main/java/com/rain/springbootstudy/WebConfig.java
+++ springbootstudy/src/main/java/com/rain/springbootstudy/WebConfig.java
@@ -3,14 +3,21 @@ package com.rain.springbootstudy;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import com.rain.springbootstudy.filter.TimeFilter;
+import com.rain.springbootstudy.intercepter.TimeInterceptor;
 
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurationSupport {
+
+	@Autowired
+	private TimeInterceptor timeInterceptor;
 
 	/**
 	 * 添加时间过滤器
@@ -32,5 +39,14 @@ public class WebConfig {
 
 		return registrationBean;
 	}
+
+	/**
+	 * 添加拦截器
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(timeInterceptor);
+	}
+
 
 }
