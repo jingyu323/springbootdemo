b78c9a9fde185e41d1d2e87b4c68573122d304be
jingyu323 <xzg323@163.com>
2018-08-09 16:25:43 +0800

    添加freemaker支持

--- springbootstudy/src/main/java/com/rain/springbootstudy/SpringbootstudyApplication.java
+++ springbootstudy/src/main/java/com/rain/springbootstudy/SpringbootstudyApplication.java
@@ -9,9 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootstudyApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(SpringbootstudyApplication.class, args);
-		
-		
 	}
 }
