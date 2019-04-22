fcf6796acd7aae4a1db9fe7f6a5805c4966873b8
Rain <xzg323@163.com>
2019-04-22 08:52:31 +0800

    复制 ribbon 项目添加 hystrix，实现熔断的功能添加

--- springclouddemo/spring-cloud-hystrix/src/main/java/com/rain/study/hystrix/SpringCloudHystrixApplication.java
+++ springclouddemo/spring-cloud-hystrix/src/main/java/com/rain/study/hystrix/SpringCloudHystrixApplication.java
@@ -1,22 +1,29 @@
-package com.rain.study.ribbon;
+package com.rain.study.hystrix;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /***
  * Spring cloud有两种服务调用方式，一种是ribbon+restTemplate，另一种是feign
+ *
+ * 本项目基本是复制service-ribbon 项目之后 添加 hystrix 包实现熔断功能
+ *
+ * hystrix 功能可以添加在 service 层 也可以添加在 contrller 层，
+ * 不过 controller 层不太好，每个调用方法都要添加
  */
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ServiceRibbonApplication {
+@EnableHystrix
+public class SpringCloudHystrixApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServiceRibbonApplication.class,
+        SpringApplication.run(SpringCloudHystrixApplication.class,
                 args);
     }
 
---
+++ springclouddemo/spring-cloud-hystrix/src/main/java/com/rain/study/hystrix/controller/HelloControler.java
@@ -0,0 +1,32 @@
+package com.rain.study.hystrix.controller;
+
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.rain.study.hystrix.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+
+
+    //增加熔断功能
+//    @HystrixCommand(fallbackMethod = "hiError")
+    @RequestMapping(value = "/sayHi")
+    public String sayHi(@RequestParam String name){
+        return helloService.sayHelloService(name);
+    }
+
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
+
+}
