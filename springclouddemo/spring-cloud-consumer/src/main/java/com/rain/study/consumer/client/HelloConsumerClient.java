e247d6f13db519c670c96c00efddadd2d908ed18
Rain <xzg323@163.com>
2019-04-22 12:14:42 +0800

    添加 fegin的 hystrix，但是有一个问题，fegin client 针对整个类 但是有咩有针对单个方法的

--- springclouddemo/spring-cloud-consumer/src/main/java/com/rain/study/consumer/client/HelloConsumerClient.java
+++ springclouddemo/spring-cloud-consumer/src/main/java/com/rain/study/consumer/client/HelloConsumerClient.java
@@ -2,6 +2,7 @@ package com.rain.study.consumer.client;
 
 
 
+import com.rain.study.consumer.fallback.SchedualServiceHiHystric;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 /***
  * Spring cloud有两种服务调用方式，一种是ribbon+restTemplate，另一种是feign
  */
-@FeignClient(name= "spring-cloud-producer")
+@FeignClient(name= "spring-cloud-producer",fallback = SchedualServiceHiHystric.class)
 public interface  HelloConsumerClient {
 
 
