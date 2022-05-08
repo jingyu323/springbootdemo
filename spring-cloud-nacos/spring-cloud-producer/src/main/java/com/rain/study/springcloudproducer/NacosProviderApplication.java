11b09b8cb87fd229ff47308a4caa38854f13b828
Rain <xzg323@163.com>
2022-05-09 07:58:27 +0800

    nacos server,  启动producer

--- spring-cloud-nacos/spring-cloud-producer/src/main/java/com/rain/study/springcloudproducer/NacosProviderApplication.java
+++ spring-cloud-nacos/spring-cloud-producer/src/main/java/com/rain/study/springcloudproducer/NacosProviderApplication.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author xiaojing
- */
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class NacosProviderApplication {
