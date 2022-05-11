c39174eb58715b3b68d314e4c66936ab0aa8096d
Rain <xzg323@163.com>
2022-05-11 08:16:30 +0800

    nacos server,  hitrx 配置引入

---
+++ spring-cloud-nacos/spring-cloud-hystrix/src/main/java/com/rain/study/hystrix/SpringCloudHystrixApplication.java
@@ -0,0 +1,54 @@
+package com.rain.study.hystrix;
+
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/***
+ * Spring cloud有两种服务调用方式，一种是ribbon+restTemplate，另一种是feign
+ *
+ * 本项目基本是复制service-ribbon 项目之后 添加 hystrix 包实现熔断功能
+ *
+ * hystrix 功能可以添加在 service 层 也可以添加在 contrller 层，
+ * 不过 controller 层不太好，每个调用方法都要添加
+ *
+ *
+ * Dashboard 后台访问地址 http://localhost:9004/hystrix.stream
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+public class SpringCloudHystrixApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudHystrixApplication.class,
+                args);
+    }
+
+    @Bean(name = "remoteRestTemplate")
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+
+        return registrationBean;
+    }
+
+}
---
+++ spring-cloud-nacos/spring-cloud-hystrix/src/main/java/com/rain/study/hystrix/service/HelloService.java
@@ -0,0 +1,28 @@
+package com.rain.study.hystrix.service;
+
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    @Qualifier(value = "remoteRestTemplate")
+    RestTemplate restTemplate;
+
+
+    //增加熔断功能
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String sayHelloService(String name) {
+        return restTemplate.getForObject("http://spring-cloud-producer/hello?name=" + name, String.class);
+    }
+
+
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
+    }
+}
