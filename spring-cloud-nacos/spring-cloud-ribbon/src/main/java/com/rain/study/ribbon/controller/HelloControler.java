e63650cdec1926a9630b2faf79f088c703dbe52b
Rain <xzg323@163.com>
2022-05-13 08:13:39 +0800

    添加ribbon

---
+++ spring-cloud-nacos/spring-cloud-ribbon/src/main/java/com/rain/study/ribbon/controller/HelloControler.java
@@ -0,0 +1,23 @@
+package com.rain.study.ribbon.controller;
+
+
+import com.rain.study.ribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+
+    @RequestMapping(value = "/sayHi")
+    public String sayHi(@RequestParam String name) {
+        return helloService.sayHelloService(name);
+    }
+
+}
