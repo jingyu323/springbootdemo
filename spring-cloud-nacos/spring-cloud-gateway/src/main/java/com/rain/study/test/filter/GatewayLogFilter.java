07a7add63835db9acbd29b62575aa3886d251057
Rain <xzg323@163.com>
2022-05-15 12:45:52 +0800

    1. 添加自定义gloable filter 打印请求头和参数

--- spring-cloud-nacos/spring-cloud-gateway/src/main/java/com/rain/study/test/filter/GatewayLogFilter.java
+++ spring-cloud-nacos/spring-cloud-gateway/src/main/java/com/rain/study/test/filter/GatewayLogFilter.java
@@ -5,10 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import org.springframework.core.io.buffer.DataBuffer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Component
@@ -17,6 +27,7 @@ public class GatewayLogFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
         System.out.println("=====================进入全局过滤器=====================");
         String token = exchange.getRequest().getQueryParams().getFirst("token");
         logger.error("token....{}", token);
@@ -25,11 +36,68 @@ public class GatewayLogFilter implements GlobalFilter, Ordered {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         }
+        String method = request.getMethodValue();
+
+        if (HttpMethod.POST.matches(method)) {
+            return DataBufferUtils.join(exchange.getRequest().getBody())
+                    .flatMap(dataBuffer -> {
+                        byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                        dataBuffer.read(bytes);
+                        String bodyString = new String(bytes, StandardCharsets.UTF_8);
+                        logtrace(exchange, bodyString);
+                        exchange.getAttributes().put("POST_BODY", bodyString);
+                        DataBufferUtils.release(dataBuffer);
+                        Flux<DataBuffer> cachedFlux = Flux.defer(() -> {
+                            DataBuffer buffer = exchange.getResponse().bufferFactory()
+                                    .wrap(bytes);
+                            return Mono.just(buffer);
+                        });
+
+                        ServerHttpRequest mutatedRequest = new ServerHttpRequestDecorator(
+                                exchange.getRequest()) {
+                            @Override
+                            public Flux<DataBuffer> getBody() {
+                                return cachedFlux;
+                            }
+                        };
+                        return chain.filter(exchange.mutate().request(mutatedRequest)
+                                .build());
+                    });
+        } else if (HttpMethod.GET.matches(method)) {
+            Map m = request.getQueryParams();
+            logtrace(exchange, m.toString());
+        }
+
+
         return chain.filter(exchange);
     }
 
     @Override
     public int getOrder() {
         return 0;
+    }
+
+    /**
+     * 日志信息
+     *
+     * @param exchange
+     * @param param    请求参数
+     */
+    private void logtrace(ServerWebExchange exchange, String param) {
+        ServerHttpRequest serverHttpRequest = exchange.getRequest();
+        String path = serverHttpRequest.getURI().getPath();
+        String method = serverHttpRequest.getMethodValue();
+        String headers = serverHttpRequest.getHeaders().entrySet()
+                .stream()
+                .map(entry -> "            " + entry.getKey() + ": [" + String.join(";", entry.getValue()) + "]")
+                .collect(Collectors.joining("\n"));
+        logger.info("\n" + "----------------             ----------------             ---------------->>\n" +
+                        "HttpMethod : {}\n" +
+                        "Uri        : {}\n" +
+                        "Param      : {}\n" +
+                        "Headers    : \n" +
+                        "{}\n" +
+                        "\"<<----------------             ----------------             ----------------"
+                , method, path, param, headers);
     }
 }
