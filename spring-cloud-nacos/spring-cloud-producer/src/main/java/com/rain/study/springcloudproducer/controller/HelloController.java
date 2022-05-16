75aa7b2488a79637a04204a255c19b0ec3d50862
Rain <xzg323@163.com>
2022-05-16 22:56:07 +0800

    1. 添加自定义参数打印

--- spring-cloud-nacos/spring-cloud-producer/src/main/java/com/rain/study/springcloudproducer/controller/HelloController.java
+++ spring-cloud-nacos/spring-cloud-producer/src/main/java/com/rain/study/springcloudproducer/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 
 /**
@@ -23,7 +24,15 @@ public class HelloController {
         System.out.println(request.getParameterMap());
 
         for (String str : request.getParameterMap().keySet()) {
-            System.out.println(str + "=" + request.getParameterMap().get(str));
+            System.out.println(str + "=" + request.getParameterValues(str));
+        }
+        Enumeration<String> paraNames = request.getParameterNames();
+        for (Enumeration e = paraNames; e.hasMoreElements(); ) {
+
+            String thisName = e.nextElement().toString();
+            String thisValue = request.getParameter(thisName);
+            System.out.println(thisName + "--------------" + thisValue);
+
         }
         return "hello " + name + "，this is first messge  from HelloController  producer， from port " + port;
     }
