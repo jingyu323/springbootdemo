9080b8f7d5f768d2844ecdd4a933f178bf4d8abb
jingyu323 <xzg323@163.com>
2022-09-24 11:21:05 +0800

    添加 utils

---
+++ springclouddemo/common-utils/src/main/java/com/rain/study/utils/NetUtils.java
@@ -0,0 +1,31 @@
+package com.rain.study.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+/**
+ * 仅仅测试网络通不通
+ *
+ */
+public class NetUtils {
+
+    private static Logger logger = LoggerFactory.getLogger(NetUtils.class);
+
+
+    public static boolean sendPingRequest(String ipAddress) throws IOException {
+        InetAddress geek = InetAddress.getByName(ipAddress);
+        logger.info("Sending Ping Request to " + ipAddress);
+        return geek.isReachable(5000);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        sendPingRequest("192.168.99.132");
+
+
+    }
+}
