e7f0cfc77107b7890f358f0e88de4c603f5756df
jingyu323 <xzg323@163.com>
2022-10-28 22:08:42 +0800

    调整引入

--- springclouddemo/common-util/src/main/java/com/rain/test/tool/utils/ip/IpUtils.java
+++ springclouddemo/common-util/src/main/java/com/rain/test/tool/utils/ip/IpUtils.java
@@ -1,8 +1,13 @@
 package com.rain.test.tool.utils.ip;
 
 
+import com.alibaba.fastjson.JSONObject;
+import com.rain.test.tool.utils.Constants;
 import com.rain.test.tool.utils.StringUtils;
 import com.rain.test.tool.utils.html.EscapeUtil;
+import com.rain.test.tool.utils.http.HttpUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
@@ -193,5 +198,40 @@ public class IpUtils
         {
         }
         return "未知";
+    }
+    private static final Logger log = LoggerFactory.getLogger(IpUtils.class);
+    // IP地址查询
+    public static final String IP_URL = "http://whois.pconline.com.cn/ipJson.jsp";
+
+    // 未知地址
+    public static final String UNKNOWN = "XX XX";
+    public static String getRealAddressByIP(String ip)
+    {
+        String address = UNKNOWN;
+        // 内网不查询
+        if (IpUtils.internalIp(ip))
+        {
+            return "内网IP";
+        }
+
+        try
+        {
+            String rspStr = HttpUtils.sendGet(IP_URL, "ip=" + ip + "&json=true", Constants.GBK);
+            if (org.apache.commons.lang3.StringUtils.isEmpty(rspStr))
+            {
+                log.error("获取地理位置异常 {}", ip);
+                return UNKNOWN;
+            }
+            JSONObject obj = JSONObject.parseObject(rspStr);
+            String region = obj.getString("pro");
+            String city = obj.getString("city");
+            return String.format("%s %s", region, city);
+        }
+        catch (Exception e)
+        {
+            log.error("获取地理位置异常 {}", ip);
+        }
+
+        return address;
     }
 }
