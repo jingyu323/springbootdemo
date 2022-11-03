da7059523a10376835c312b43e83e4c7c1983c74
jingyu323 <xzg323@163.com>
2022-11-03 23:06:20 +0800

    threadoppl

---
+++ springclouddemo/threadpooldemo/src/test/java/com/rain/test/threadpooldemo/ThreadpooldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rain.test.threadpooldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadpooldemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
