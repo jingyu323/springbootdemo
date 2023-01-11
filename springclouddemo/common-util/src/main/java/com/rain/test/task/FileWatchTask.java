3f52595a3b2807b6ec7c558b92d4f700f38b5ee6
jingyu323 <xzg323@163.com>
2023-01-11 14:50:29 +0800

    file motnitor

---
+++ springclouddemo/common-util/src/main/java/com/rain/test/task/FileWatchTask.java
@@ -0,0 +1,63 @@
+package com.rain.test.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+public class FileWatchTask  implements Runnable{
+
+    public String fileDirectory;
+    public static final Logger logger = LoggerFactory.getLogger(FileWatchTask.class);
+
+    public FileWatchTask(String fileDirectory) {
+        this.fileDirectory = fileDirectory;
+    }
+
+
+
+    public void run() {
+        logger.info("watch is start....");
+        WatchService service = null;
+        try {
+            // 获取当前文件系统的监控对象
+            service = FileSystems.getDefault().newWatchService();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            // 获取文件目录下的Path对象注册到 watchService中。
+            // 监听的事件类型，有创建，删除，以及修改
+            Paths.get(fileDirectory).register(service, StandardWatchEventKinds.ENTRY_CREATE,
+                    StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            logger.info("watch22222 is start....");
+            WatchKey key = null;
+            try {
+                // 获取可用key.没有可用的就wait
+                key = service.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            for (WatchEvent<?> event : key.pollEvents()) {
+                // todo
+                logger.info(event.context() + "文件:" + event.kind() + "次数: " + event.count());
+            }
+            // 重置，这一步很重要，否则当前的key就不再会获取将来发生的事件
+            boolean valid = key.reset();
+            // 失效状态，退出监听
+            if (!valid) {
+                break;
+
+
+            }
+            logger.info("dddddddddddddd....................");
+        }
+
+    }
+
+}
