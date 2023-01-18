5ad90181441d61251ce6cafe4fbae56d357f24fe
jingyu323 <xzg323@163.com>
2023-01-18 10:18:47 +0800

    eurcat client

--- springclouddemo/eureka-client2/src/main/java/com/rain/study/client/EurekaClientApplication.java
+++ springclouddemo/eureka-client2/src/main/java/com/rain/study/client/EurekaClientApplication.java
@@ -1,13 +1,9 @@
-package com.rain.study.ribbon;
+package com.rain.study.client;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /***
  * Spring cloud有两种服务调用方式，一种是ribbon+restTemplate，另一种是feign
