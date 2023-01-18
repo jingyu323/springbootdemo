9eb080a253c96faf11c1523655896eca0edca2d2
jingyu323 <xzg323@163.com>
2023-01-18 21:38:37 +0800

    client 2

---
+++ springclouddemo/service-ribbon2/src/main/java/com/rain/study/ribbon/ServiceRibbonApplication.java
@@ -0,0 +1,29 @@
+package com.rain.study.ribbon;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/***
+ * Spring cloud有两种服务调用方式，一种是ribbon+restTemplate，另一种是feign
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ServiceRibbonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class,
+                args);
+    }
+
+    @Bean(name="remoteRestTemplate")
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
