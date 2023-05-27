2ac68244183f63ff864b5f6892e1c8bf6f61430c
jingyu323 <xzg323@163.com>
2023-05-27 11:06:03 +0800

    mongo

---
+++ mongo_study/src/main/java/MongoDBJDBC.java
@@ -0,0 +1,36 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import org.bson.Document;
+
+public class MongoDBJDBC {
+    private final Logger logger = LoggerFactory.getLogger(MongoDBJDBC.class);
+    public static void main(String[] args) {
+        try{
+            // 连接到 mongodb 服务
+            MongoClient mongoClient = new MongoClient( "192.168.100.38" , 27017 );
+
+            // 连接到数据库
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("raintest");
+            System.out.println("Connect to database successfully");
+
+
+            //3.获取集合
+            MongoCollection<Document> collection = mongoDatabase.getCollection("raintest");
+            //4.查询获取文档集合
+            FindIterable<Document> documents = collection.find();
+            //5.循环遍历
+            for (Document document : documents) {
+                System.out.println(document);
+            }
+            //6.关闭连接
+            mongoClient.close();
+
+        }catch(Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+    }
+}
---
+++ mongo_study/src/main/java/Mongodb.java
@@ -0,0 +1,26 @@
+
+
+import org.bson.Document;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+
+public class Mongodb {
+    public static void mongoQueryAll() {
+        //1.创建链接
+        MongoClient client = new MongoClient( "192.168.100.38" , 27017 );
+        //2.打开数据库test
+        MongoDatabase db = client.getDatabase("raintest");
+        System.out.println("sss");
+        System.out.println("Connect to database successfully");
+        db.createCollection("test22222");
+        System.out.println("集合创建成功");
+    }
+
+    public static void main(String[] args) {
+        mongoQueryAll();
+    }
+}
