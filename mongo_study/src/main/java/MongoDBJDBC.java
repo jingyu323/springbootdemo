7f46819ab80f5ad90886b2eae99f6ae15783c90f
Rain <xzg323@163.com>
2023-05-28 18:48:37 +0800

    mongo

--- mongo_study/src/main/java/MongoDBJDBC.java
+++ mongo_study/src/main/java/MongoDBJDBC.java
@@ -2,7 +2,7 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
+import org.bson.Document;
 
 
 public class MongoDBJDBC {
--- mongo_study/src/main/java/Mongodb.java
+++ mongo_study/src/main/java/Mongodb.java
@@ -1,21 +1,34 @@
 
 
+import com.mongodb.ConnectionString;
 import com.mongodb.MongoClient;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.*;
+
+import java.util.List;
 
 
 public class Mongodb {
     public static void mongoQueryAll() {
+//
+
+
+
         //1.创建链接
-        MongoClient client = new MongoClient( "192.168.182.142" , 27017 );
+        MongoClient client = new MongoClient( "192.168.182.142" , 27017  );
+
+        MongoIterable<String> databaseNames = client.listDatabaseNames();
+        for (String s : databaseNames) {
+            System.out.println(s);
+        }
+
+
         //2.打开数据库test
-        MongoDatabase db = client.getDatabase("raintest");
-        System.out.println("sss");
-        System.out.println("Connect to database successfully");
-        db.createCollection("test22222");
-        System.out.println("集合创建成功");
+//        MongoDatabase db = client.getDatabase("raintest");
+//        System.out.println("sss");
+//        System.out.println("Connect to database successfully");
+//        db.createCollection("test22222");
+//        System.out.println("集合创建成功");
     }
 
     public static void main(String[] args) {
