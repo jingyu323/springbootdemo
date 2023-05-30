9369263aef870b4308d9fa3c52383e18f6a2230a
Rain <xzg323@163.com>
2023-05-30 22:21:35 +0800

    insert one

--- mongo_study/src/main/java/com/rain/insert/AddOneFild.java
+++ mongo_study/src/main/java/com/rain/insert/AddOneFild.java
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.UpdateResult;
 import org.bson.Document;
@@ -14,21 +15,23 @@ import org.slf4j.LoggerFactory;
 
 import static com.mongodb.client.model.Filters.gt;
 
-public class UpdateMany {
-    private final static Logger logger = LoggerFactory.getLogger(UpdateMany.class);
+public class AddOneFild {
+    private final static Logger logger = LoggerFactory.getLogger(AddOneFild.class);
 
     public static void main(String[] args) {
         String uri = "mongodb://192.168.182.142:27017";
         try (MongoClient mongoClient = MongoClients.create(uri)) {
             MongoDatabase database = mongoClient.getDatabase("sample_mflix");
             MongoCollection<Document> collection = database.getCollection("movies");
-            Bson query = null;
+            Bson query = gt("num_mflix_comments", 50);
             Bson updates = Updates.combine(
+                    Updates.set("num_mflix_comments", 60),
                     Updates.addToSet("genres", "Frequently Discussed"),
                     Updates.currentTimestamp("lastUpdated"));
 
+            UpdateOptions options = new UpdateOptions().upsert(true);
             try {
-                UpdateResult result = collection.updateMany(query, updates);
+                UpdateResult result = collection.updateMany(null, updates, options);
                 logger.info("Modified document count: " + result.getModifiedCount());
             } catch (MongoException me) {
                 logger.info("Unable to update due to an error: " + me);
