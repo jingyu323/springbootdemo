5a2f5f70531637463930b05550d8925f30bc5220
Rain <xzg323@163.com>
2023-05-30 22:58:06 +0800

    insert one

---
+++ mongo_study/src/main/java/com/rain/insert/DeleteMany.java
@@ -0,0 +1,32 @@
+package com.rain.insert;
+
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.lt;
+
+public class DeleteMany {
+    public static void main(String[] args) {
+        // Replace the uri string with your MongoDB deployment's connection string
+        String uri = "mongodb://192.168.182.142:27017";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+            Bson query = lt("num_mflix_comments", 50);
+            try {
+                DeleteResult result = collection.deleteMany(query);
+                System.out.println("Deleted document count: " + result.getDeletedCount());
+            } catch (MongoException me) {
+                System.err.println("Unable to delete due to an error: " + me);
+            }
+        }
+    }
+
+}
---
+++ mongo_study/src/main/java/com/rain/insert/DeleteOne.java
@@ -0,0 +1,30 @@
+package com.rain.insert;
+
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import static com.mongodb.client.model.Filters.eq;
+
+public class DeleteOne {
+    public static void main(String[] args) {
+        // Replace the uri string with your MongoDB deployment's connection string
+        String uri = "mongodb://192.168.182.142:27017";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+            Bson query = eq("title", "The Garbage Pail Kids Movie");
+            try {
+                DeleteResult result = collection.deleteOne(query);
+                System.out.println("Deleted document count: " + result.getDeletedCount());
+            } catch (MongoException me) {
+                System.err.println("Unable to delete due to an error: " + me);
+            }
+        }
+    }
+}
