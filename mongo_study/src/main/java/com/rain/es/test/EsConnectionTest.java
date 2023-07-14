e3ae8bff87c20cc4abe38d7995a42f5f3000e0da
jingyu323 <xzg323@163.com>
2023-07-14 19:34:24 +0800

    es

--- mongo_study/src/main/java/com/rain/es/test/EsConnectionTest.java
+++ mongo_study/src/main/java/com/rain/es/test/EsConnectionTest.java
@@ -1,41 +1,33 @@
 package com.rain.es.test;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.TransportUtils;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
-import com.sun.xml.internal.ws.api.ResourceLoader;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
-import sun.misc.Resource;
 
 import javax.net.ssl.SSLContext;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class EsConnectionTest {
     public static void main(String[] args) throws IOException {
-        URL resourceUrl = EsConnectionTest.class.getClassLoader().getResource("certs/http_ca.crt");
 
-        InputStream inputStream = EsConnectionTest.class.getResourceAsStream("/certs/http_ca.crt");
-
-
-        File certFile = new File(resourceUrl.getPath());
 
         SSLContext sslContext = TransportUtils
-                .sslContextFromHttpCaCrt(certFile);
+                .sslContextFromHttpCaCrt(new File("src/main/java/com/rain/es/test/http_ca.crt"));
         BasicCredentialsProvider credsProv = new BasicCredentialsProvider();
         credsProv.setCredentials(
                 AuthScope.ANY, new UsernamePasswordCredentials("elastic", "V6DycNLQVPeYKhBLPZUO")
         );
-        String serverUrl = "https://192.168.99.118:9200";
+
 
         RestClient restClient = RestClient
                 .builder(new HttpHost("192.168.99.118", 9200, "https"))
@@ -49,9 +41,24 @@ public class EsConnectionTest {
         ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
         ElasticsearchClient esClient = new ElasticsearchClient(transport);
 
-        esClient.indices().create(c -> c
-                .index("products")
-        );
+//        esClient.indices().create(c -> c
+//                .index("products")
+//        );
+
+
+        SearchResponse<Product> search = esClient.search(s -> s
+                        .index("products")
+                        .query(q -> q
+                                .term(t -> t
+                                        .field("name")
+                                        .value(v -> v.stringValue("bicycle"))
+                                )),
+                Product.class);
+
+        for (Hit<Product> hit : search.hits().hits()) {
+            System.out.println(hit.id());
+
+        }
 
     }
 }
