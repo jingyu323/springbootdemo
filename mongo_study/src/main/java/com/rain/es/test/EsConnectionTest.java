dc7a9e1116fccfc185c664f8c9e26e58d367b2ef
jingyu323 <xzg323@163.com>
2023-07-14 21:02:17 +0800

    es

--- mongo_study/src/main/java/com/rain/es/test/EsConnectionTest.java
+++ mongo_study/src/main/java/com/rain/es/test/EsConnectionTest.java
@@ -1,7 +1,35 @@
 package com.rain.es.test;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.message.BasicHeader;
+import org.elasticsearch.client.RestClient;
+
 public class EsConnectionTest {
     public static void main(String[] args) {
+
+        // URL and API key
+        String serverUrl = "https://localhost:9200";
+        String apiKey = "VnVhQ2ZHY0JDZGJrU...";
+
+// Create the low-level client
+        RestClient restClient = RestClient
+                .builder(HttpHost.create(serverUrl))
+                .setDefaultHeaders(new Header[]{
+                        new BasicHeader("Authorization", "ApiKey " + apiKey)
+                })
+                .build();
+
+// Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+// And create the API client
+        ElasticsearchClient esClient = new ElasticsearchClient(transport);
         
     }
 }
