a5559da7f8f0a91d3e23c18154b38def9fdbbbaf
jingyu323 <xzg323@163.com>
2023-11-09 15:36:09 +0800

    pwd

---
+++ mysql_study/src/main/java/com/rain/test/common/LogUtils.java
@@ -0,0 +1,16 @@
+package com.rain.test.common;
+
+public class LogUtils {
+    public static void info(String context) {
+        System.out.println(context);
+    }
+
+    public static void warn(String context) {
+        System.out.println(context);
+    }
+
+    public static void error(String context) {
+        System.out.println(context);
+    }
+ 
+}
