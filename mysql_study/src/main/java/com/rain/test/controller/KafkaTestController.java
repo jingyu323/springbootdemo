71421fea8db24e78ec43fb20c04f09e2cfb5147d
jingyu323 <xzg323@163.com>
2023-11-27 10:19:26 +0800

    kafka

--- mysql_study/src/main/java/com/rain/test/controller/KafkaTestController.java
+++ mysql_study/src/main/java/com/rain/test/controller/KafkaTestController.java
@@ -1,6 +1,7 @@
 package com.rain.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
