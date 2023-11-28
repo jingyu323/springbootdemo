acdc1cac64dafaebe69fb321864bc220524f354e
jingyu323 <xzg323@163.com>
2023-11-28 16:21:48 +0800

    kafka

--- mysql_study/readme.md
+++ mysql_study/readme.md
@@ -1,3 +1,33 @@
 # PDF
 
 https://blog.csdn.net/chengp919/article/details/78040823
+
+```java
+
+@KafkaListener(....)
+public void listen1(String data)
+
+@KafkaListener(....)
+public void listen2(ConsumerRecord<K, V> data)
+
+@KafkaListener(....)
+public void listen3(ConsumerRecord<K, V> data,Acknowledgment acknowledgment)
+
+@KafkaListener(....)
+public void listen4(ConsumerRecord<K, V> data,
+        Acknowledgment acknowledgment,Consumer<K, V> consumer)
+
+@KafkaListener(....)
+public void listen5(List<String> data)
+
+@KafkaListener(....)
+public void listen6(List<ConsumerRecord<K, V>>data)
+
+@KafkaListener(....)
+public void listen7(List<ConsumerRecord<K, V>>data,Acknowledgment acknowledgment)
+
+@KafkaListener(....)
+public void listen8(List<ConsumerRecord<K, V>>data,
+
+
+```
--- mysql_study/src/main/java/com/rain/test/MysqlStudyApplication.java
+++ mysql_study/src/main/java/com/rain/test/MysqlStudyApplication.java
@@ -1,13 +1,20 @@
 package com.rain.test;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.json.JsonGeneratorImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rain.test.thread.MsgConsumer;
+import kafka.utils.Json;
+import kafka.utils.json.JsonArray;
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -15,22 +22,32 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.Properties;
+import javax.annotation.Resource;
 
 
 @SpringBootApplication
-//@EnableKafka
+@EnableKafka
 @ComponentScan("com.rain.test.*")
 public class MysqlStudyApplication {
+
+    @Autowired
+    private KafkaProperties properties;
+    @Resource
+    private ObjectMapper objectMapper;
+
     @Component
     public class ConsumerListener {
         @KafkaListener(topics = "test", groupId = "group22222")
-        public void onMessage(String msg) {
+        public void onMessage(String msg, Consumer<String, String> consumer) {
             System.out.println("----收到消息：" + msg + "----");
 
+            try {
+                System.out.println(objectMapper.writeValueAsString(properties));
 
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            consumer.commitAsync();
         }
     }
 
@@ -39,7 +56,7 @@ public class MysqlStudyApplication {
         @KafkaListener(topics = "test", groupId = "group2333")
         public void onMessage(ConsumerRecord<String, String> record) {
             System.out.println("分区2 ：" + record.topic() + " : " + record.value());
-            
+
         }
     }
 
@@ -58,7 +75,17 @@ public class MysqlStudyApplication {
     public static void main(String[] args) {
         Thread task = new Thread(new MsgConsumer());
         task.start();
+
+
         SpringApplication.run(MysqlStudyApplication.class, args);
 
     }
+
+    /**
+     *
+     * properties:
+     * {"bootstrapServers":["localhost:9092"],"clientId":null,"properties":{},"consumer":{"ssl":{"keyPassword":null,"keyStoreCertificateChain":null,"keyStoreKey":null,"keyStoreLocation":null,"keyStorePassword":null,"keyStoreType":null,"trustStoreCertificates":null,"trustStoreLocation":null,"trustStorePassword":null,"trustStoreType":null,"protocol":null},"security":{"protocol":null},"autoCommitInterval":"PT1S","autoOffsetReset":"latest","bootstrapServers":null,"clientId":null,"enableAutoCommit":false,"fetchMaxWait":null,"fetchMinSize":null,"groupId":"default-group","heartbeatInterval":null,"isolationLevel":"READ_UNCOMMITTED","keyDeserializer":"org.apache.kafka.common.serialization.StringDeserializer","valueDeserializer":"org.apache.kafka.common.serialization.StringDeserializer","maxPollRecords":500,"properties":{}},"producer":{"ssl":{"keyPassword":null,"keyStoreCertificateChain":null,"keyStoreKey":null,"keyStoreLocation":null,"keyStorePassword":null,"keyStoreType":null,"trustStoreCertificates":null,"trustStoreLocation":null,"trustStorePassword":null,"trustStoreType":null,"protocol":null},"security":{"protocol":null},"acks":"1","batchSize":{"negative":false},"bootstrapServers":null,"bufferMemory":{"negative":false},"clientId":null,"compressionType":null,"keySerializer":"org.apache.kafka.common.serialization.StringSerializer","valueSerializer":"org.apache.kafka.common.serialization.StringSerializer","retries":3,"transactionIdPrefix":null,"properties":{"linger.ms":"5  "}},"admin":{"ssl":{"keyPassword":null,"keyStoreCertificateChain":null,"keyStoreKey":null,"keyStoreLocation":null,"keyStorePassword":null,"keyStoreType":null,"trustStoreCertificates":null,"trustStoreLocation":null,"trustStorePassword":null,"trustStoreType":null,"protocol":null},"security":{"protocol":null},"clientId":null,"properties":{},"failFast":false},"streams":{"ssl":{"keyPassword":null,"keyStoreCertificateChain":null,"keyStoreKey":null,"keyStoreLocation":null,"keyStorePassword":null,"keyStoreType":null,"trustStoreCertificates":null,"trustStoreLocation":null,"trustStorePassword":null,"trustStoreType":null,"protocol":null},"security":{"protocol":null},"cleanup":{"onStartup":false,"onShutdown":false},"applicationId":null,"autoStartup":true,"bootstrapServers":null,"cacheMaxSizeBuffering":null,"clientId":null,"replicationFactor":null,"stateDir":null,"properties":{}},"listener":{"type":"SINGLE","ackMode":"MANUAL_IMMEDIATE","clientId":null,"concurrency":null,"pollTimeout":null,"noPollThreshold":null,"ackCount":null,"ackTime":null,"idleBetweenPolls":"PT0S","idleEventInterval":null,"monitorInterval":null,"logContainerConfig":null,"onlyLogRecordMetadata":true,"missingTopicsFatal":false},"ssl":{"keyPassword":null,"keyStoreCertificateChain":null,"keyStoreKey":null,"keyStoreLocation":null,"keyStorePassword":null,"keyStoreType":null,"trustStoreCertificates":null,"trustStoreLocation":null,"trustStorePassword":null,"trustStoreType":null,"protocol":null},"jaas":{"enabled":false,"loginModule":"com.sun.security.auth.module.Krb5LoginModule","controlFlag":"REQUIRED","options":{}},"template":{"defaultTopic":"test"},"security":{"protocol":null}}
+     */
+
+
 }
