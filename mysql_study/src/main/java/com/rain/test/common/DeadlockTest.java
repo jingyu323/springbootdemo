d9d64fd0a3a891826d2a0eb3ef60a92737675297
jingyu323 <xzg323@163.com>
2024-01-10 15:43:56 +0800

    java  video

--- mysql_study/src/main/java/com/rain/test/common/DeadlockTest.java
+++ mysql_study/src/main/java/com/rain/test/common/DeadlockTest.java
@@ -1,28 +1,7 @@
-package com.rain.test;
+package com.rain.test.common;
 
-import org.junit.jupiter.api.Test;
-
-import java.io.File;
-import java.util.Random;
-
-public class BasicTest {
-
-
-    @Test
-    public void testRand() {
-
-        System.out.println("Byte.SIZE=" + Byte.SIZE / 8);
-        System.out.println("Character.SIZE=" + Character.SIZE / 8);
-        System.out.println("Short.SIZE=" + Short.SIZE / 8);
-        System.out.println("Integer.SIZE=" + Integer.SIZE / 8);
-        System.out.println("Long.SIZE=" + Long.SIZE / 8);
-        System.out.println("Float.SIZE=" + Float.SIZE / 8);
-        System.out.println("Double.SIZE=" + Double.SIZE / 8);
-    }
-
-    @Test
-    public void deadlockTest() {
-
+public class DeadlockTest {
+    public static void main(String[] args) {
         Object object1 = new Object();
         Object object2 = new Object();
         Thread t1 = new Thread(() -> {
@@ -46,5 +25,4 @@ public class BasicTest {
         }, "deadlock-demo-2");
         t2.start();
     }
-
 }
