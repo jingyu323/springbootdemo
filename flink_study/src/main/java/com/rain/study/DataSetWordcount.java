b56fa8bcb70a9a8c014f7a655237fcee5b01caa0
jingyu323 <xzg323@163.com>
2024-11-14 18:06:10 +0800

    flink

--- flink_study/src/main/java/com/rain/study/DataSetWordcount.java
+++ flink_study/src/main/java/com/rain/study/DataSetWordcount.java
@@ -1,10 +1,10 @@
 package com.rain.study;
 
+
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.util.Collector;
 
 public class DataSetWordcount {
