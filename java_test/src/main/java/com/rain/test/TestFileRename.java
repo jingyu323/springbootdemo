0a31e8fb4b8043928261c3a9e827b960a66a097c
jingyu323 <xzg323@163.com>
2025-02-22 16:58:43 +0800

    文件夹重命名

--- java_test/src/main/java/com/rain/test/TestFileRename.java
+++ java_test/src/main/java/com/rain/test/TestFileRename.java
@@ -4,6 +4,23 @@ import java.io.File;
 
 public class TestFileRename {
     public static void main(String[] args) {
+        getFileList();
+    }
+    public  static  void getFileList(){
+        String filePath2 = "E:\\home\\htkj\\dataLabelRootPath\\testLabel001";
+        File file = new File(filePath2);
+        File[] files = file.listFiles();
+        for(File f:files){
+            if(f.isDirectory()){
+                File[] subfiles = f.listFiles();
+                for(File subfile:subfiles){
+                    System.out.println(subfile.getAbsoluteFile());
+                }
+            }
+            System.out.println(f.getAbsoluteFile());
+        }
+    }
+    public static void renameFileFolder(){
         String filePath = "d:/sss111";
         String filePath2 = "d:/sss222";
 
