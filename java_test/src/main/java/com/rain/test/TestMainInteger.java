349ffd0e8872ebd334ee314a26716ffc033e9d46
jingyu323 <xzg323@163.com>
2025-10-31 16:22:23 +0800

    测试代码

--- java_test/src/main/java/com/rain/test/TestMainInteger.java
+++ java_test/src/main/java/com/rain/test/TestMainInteger.java
@@ -4,6 +4,24 @@ public class TestMainInteger {
     public static void main(String[] args) {
 
 
-        System.out.println(Integer.valueOf("09"));
+
+
+        Long l = Long.valueOf("998898");
+        Long l1 = Long.valueOf("1000");
+        Long l2= Long.valueOf("888888");
+
+        System.out.println(l/1000.0);
+
+       String res = formateDouble(l/1000.0);
+
+        System.out.println(res);
+
+
+
+    }
+
+    private static String formateDouble(double number) {
+        String formattedNumber = String.format("%.2f", number);
+        return formattedNumber;
     }
 }
---
+++ java_test/src/main/java/com/rain/test/TraingParse.java
@@ -0,0 +1,27 @@
+package com.rain.test;
+
+import com.alibaba.fastjson.JSON;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.List;
+
+public class TraingParse {
+
+    public static void main(String[] args) throws IOException {
+ 
+        
+
+
+
+
+
+
+
+
+    }
+}
